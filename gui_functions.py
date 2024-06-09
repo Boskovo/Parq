cd11f4730574b91bb1378eee8f5d7fd50bc8cd2f
ricardobos <68953190+Boskovo@users.noreply.github.com>
2024-06-09 14:50:03 +0200

    returns to main page but it still has bugs

--- gui_functions.py
+++ gui_functions.py
@@ -4,7 +4,11 @@ import platform
 
 #function that changes the frame (page) of the application
 def change_frame(old_frame,new_frame):
-    
+
+    #define the frames as variables for use by other functions
+    change_frame.old_frame = old_frame
+    change_frame.new_frame = new_frame
+
     #remove old frame from grid and add new frame
     old_frame.grid_forget()
     new_frame.grid(row=0, column=0, sticky="nsew")
@@ -21,6 +25,8 @@ def play_video(frame):
     # creating a media player object
     media_player = player.media_player_new()
 
+    play_video.media_player = media_player
+
     #connecting the media player object to the frame (conditional logic for ease of use on different platforms)
     if platform.system() == 'Windows':
         media_player.set_hwnd(frame.winfo_id())
@@ -29,7 +35,26 @@ def play_video(frame):
 
     #set the video to the media player
     media_player.set_media(media) 
-    
-    # start playing video 
-    media_player.play() 
 
+    #define the manager for the media player
+    manager = media_player.event_manager()
+
+    # start playing video 
+    media_player.play()
+
+    #detect an EndReached event and call the end_reached function
+    manager.event_attach(vlc.EventType.MediaPlayerEndReached, end_reached)
+
+def end_reached(event):
+
+    media_player = play_video.media_player
+
+    #media_player.stop()
+    #media_player.get_media().release()
+    #media_player.release()
+    #media_player.get_instance.release()
+
+    change_frame.new_frame.grid_forget()
+    change_frame.old_frame.grid(row=0, column=0, sticky="nsew")
+    
+    print('end reached')
