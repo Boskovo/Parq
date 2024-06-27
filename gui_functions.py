67e5bc39f49a3a3f38cbe954b706a6db36d046fa
Boskovo <ricardobos22@gmai.com>
2024-06-27 13:08:30 +0200

    more attempts to improve preformance a slight bit

--- gui_functions.py
+++ gui_functions.py
@@ -30,9 +30,10 @@ def play_video(frame,video_type):
     stop_event.clear()
 
     options = [
-    '--codec=avcodec',      # Use avcodec for decoding
+    '--codec', 'h264_v4l2m2m',      # Use avcodec for decoding
     '--avcodec-hw=any',     # Enable hardware acceleration so the video is decoded on the GPU
     '--avcodec-skiploopfilter=4',  # Decrease cpu load by skipping loop filters on non-reference frames
+    '--file-caching=3000',      # Adjust caching to improve playback
     '--no-xlib',            # Disable Xlib to avoid issues on RPi
     ]      
 
